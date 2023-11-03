d3fe283c7babd625ea4c0afa3043b0afd341f06b
Varsha <varsha@gmail.com>
2023-11-03 16:48:37 +0530

    File added

--- Test.java
+++ Test.java
@@ -1,17 +1,12 @@
-package Array;
-
 class Test {
 
-	public static void main(String[] args) {
-		int[] a = {10,20,30};
-		for(int i = 0 ; i < a.length ; i++) {
-			System.out.println(a[i]);
-		}
-		System.out.println("---------------");
-		String[] fruits = {"Apple","Banana","Mango"};
-		for(int i = 0 ; i < fruits.length ; i++) {
-			System.out.println(fruits[i]);
-		}
-	}
+  public static void main(String[] args) {
 
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
 }
